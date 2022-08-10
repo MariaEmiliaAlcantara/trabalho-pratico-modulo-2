a0ca1862a2146ac96a8d1c18e6a078e913390705
MariaSchinoni <maria.schinoni10@gmail.com>
2022-08-10 14:58:21 -0300

    change color if yield is positive or negative

--- src/components/Investment.jsx
+++ src/components/Investment.jsx
@@ -1,16 +1,7 @@
 const Investment = ({ children }) => {
   return (
     <>
-      <table className="container mx-auto p-10 m-5">
-        <thead>
-          <tr>
-            <th>Mês</th>
-            <th>Saldo</th>
-            <th>Rentabilidade</th>
-          </tr>
-        </thead>
-      </table>
-      <div className="container mx-auto p-10 m-5">{children}</div>
+      <div className="container mx-auto">{children}</div>
     </>
   );
 };
--- src/components/InvestmentItem.jsx
+++ src/components/InvestmentItem.jsx
@@ -1,16 +1,42 @@
 const InvestmentItem = ({ data }) => {
   return (
-    <table className="container mx-auto p-10 m-5">
-      <thead>
-        {data.reports.map((report) => (
-          <tr key={report.id}>
-            <th>{`${report.month}/${report.year}`}</th>
-            <th>{report.value.toFixed(2)}</th>
-            <th>rentabilidade</th>
+    <>
+      <h1 className="text-center m-2">{data.description} </h1>
+      <h3
+        className={`text-center m-2 ${
+          data.rentabilidadeAnual >= 0 ? "text-green-800" : "text-red-800"
+        }
+        `}
+      >
+        (Rentabilidade Anual: {data.rentabilidadeAnual}%)
+      </h3>
+      <table className="container mx-auto p-10 mb-5 text-sm text-base">
+        <thead>
+          <tr>
+            <th>Mês</th>
+            <th>Saldo</th>
+            <th>Rentabilidade</th>
           </tr>
+        </thead>
+        {data.reports.map((report) => (
+          <tbody>
+            <tr key={report.id}>
+              <th>{`${report.month}/${report.year}`}</th>
+              <th>R${report.value.toFixed(2)}</th>
+              <th
+                className={
+                  report.rentabilidadeMensal >= 0
+                    ? "text-green-800"
+                    : "text-red-800"
+                }
+              >
+                {report.rentabilidadeMensal}%
+              </th>
+            </tr>
+          </tbody>
         ))}
-      </thead>
-    </table>
+      </table>
+    </>
   );
 };
 
